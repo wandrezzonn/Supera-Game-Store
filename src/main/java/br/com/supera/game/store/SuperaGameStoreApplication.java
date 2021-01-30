48bdcc7150a92362d0ff3ca1be693229ba507c78
Anderson <anderson4602@gmail.com>
2021-01-30 13:31:02 -0300

    Criado a estrutura inicial de pacote do projeto

---
+++ src/main/java/br/com/supera/game/store/SuperaGameStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.supera.game.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperaGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperaGameStoreApplication.class, args);
+	}
+
+}
